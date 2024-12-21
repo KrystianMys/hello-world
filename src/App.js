21ae2a3a64ed8080c7b9c3801859cdfab43ebbef
KrystianMys <krystian@smartercontact.com>
2024-12-21 23:47:38 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
-import { Provider } from "@/components/ui/provider";
 import HomePage from "./HomePage";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Todo from "./Todo";
 
 function App({ Component, pageProps }) {
